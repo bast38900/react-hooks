156d7c2f8e7aacaeefdd33f57c6821924163ece9
bast38900 <bast38900@edu.ucl.dk>
2023-04-18 11:32:30 +0200

    EditUser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
 import AddUserForm from "./forms/AddUserForm";
+import EditUserForm from "./forms/EditUserForm";
 
 const App = () => {
   // Function to add "Dummy data"
@@ -11,7 +12,11 @@ const App = () => {
   ];
 
   // Implementing State Hook, to change state of users
+  // Setting the initial state, when editing users
   const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, name: "", username: "" };
+  const [currentUser, setCurrentUser] = useState(initialFormState);
 
   // CREATE function to add users
   const addUser = (user) => {
@@ -21,7 +26,23 @@ const App = () => {
 
   // DELETE function to delete users
   const deleteUser = (id) => {
+    setEditing(false);
+
     setUsers(users.filter((user) => user.id !== id));
+  };
+
+  // Switch to entering edit mode, when pressing edit in view
+  const editRow = (user) => {
+    setEditing(true);
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+
+  // EDIT function to edit users
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
   };
 
   return (
@@ -29,12 +50,25 @@ const App = () => {
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser} />
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
